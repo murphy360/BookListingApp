d4a9e49ac09c383ab08ead7c921d17999b0c0dd6
Corey Murphy <c.murphy360@gmail.com>
2016-12-29 20:10:01 -0800

    Clean Code!

--- app/src/main/java/com/andrios/booklistingapp/BookLoader.java
+++ app/src/main/java/com/andrios/booklistingapp/BookLoader.java
@@ -9,12 +9,12 @@ import java.util.ArrayList;
  * Created by Corey on 11/27/2016.
  */
 
-public class BookLoader extends AsyncTaskLoader<ArrayList<Book>> {
+class BookLoader extends AsyncTaskLoader<ArrayList<Book>> {
 
     private String mUrl;
     final static String TAG = "BookLoader";
 
-    public BookLoader(Context context, String url) {
+    BookLoader(Context context, String url) {
         super(context);
         this.mUrl = url;
     }
@@ -26,7 +26,6 @@ public class BookLoader extends AsyncTaskLoader<ArrayList<Book>> {
 
     @Override
     public ArrayList<Book> loadInBackground() {
-        ArrayList<Book> books = QueryUtils.extractBooks(mUrl);
-        return books;
+        return QueryUtils.extractBooks(mUrl);
     }
 }
